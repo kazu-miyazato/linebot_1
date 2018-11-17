8dfe06b6c4f7ec6014629c8d6080581bbc420292
Miyazato Kazuhiro <KAZUHIRO@Miyazato-no-MacBook-Pro.local>
2018-11-17 19:41:55 +0900

    create new file

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php':
+
+$httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new ¥LINE¥LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+
+foreach ($events as $event) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
+  }
+  $bot->replyText($event->getReplyToken(), $event->getText());
+}
+
+?>
