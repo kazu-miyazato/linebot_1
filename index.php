a36c623479041ba3dbfaa8f04a8894b0cd4bb42c
Miyazato Kazuhiro <KAZUHIRO@Miyazato-no-MacBook-Pro.local>
2018-11-17 20:28:42 +0900

    fix the index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php':
+require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new ¥LINE¥LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
