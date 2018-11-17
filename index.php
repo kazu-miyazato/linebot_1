3056be4a8e0665850ba2a1c9cac6c53d69600d57
Miyazato Kazuhiro <KAZUHIRO@Miyazato-no-MacBook-Pro.local>
2018-11-17 21:18:08 +0900

    fix and fix and fix the index.php

--- index.php
+++ index.php
@@ -2,28 +2,28 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new ¥LINE¥LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-$signature = $_SERVER["HTTP_" . ¥LINE¥LINEBot¥Constant¥HTTPHeader::LINE_SIGNATURE];
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(¥LINE¥LINEBot¥Exception¥InvalidSignatureException $e) {
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
   error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
-} catch(¥LINE¥LINEBot¥Exception¥UnknownEventTypeException $e) {
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
   error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
-} catch(¥LINE¥LINEBot¥Exception¥UnknownMessageTypeException $e) {
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
   error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
-} catch(¥LINE¥LINEBot¥Exception¥InvalidEventRequestException $e) {
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
 }
 
 foreach ($events as $event) {
-  if (!($event instanceof ¥LINE¥LINEBot¥Event¥MessageEvent)) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
     continue;
   }
-  if (!($event instanceof ¥LINE¥LINEBot¥Event¥MessageEvent¥TextMessage)) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
     error_log('Non text message has come');
     continue;
   }
